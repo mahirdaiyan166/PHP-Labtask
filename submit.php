5306fd6f3bd3ae375e97102a3c25b0576b2e9680
Mahir Daiyan <ndaiyan45@gmail.com>
2019-10-17 02:40:11 +0600

    First commit

---
+++ submit.php
@@ -0,0 +1,15 @@
+<?php
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$date = $_POST['date'];
+	$gender = $_POST['gender'];
+	$degree = $_POST['option'];
+	$blood = $_POST['a'];
+	echo $name."<br>";
+	echo $email."<br>";
+	echo $date."<br>";
+	echo $gender."<br>";
+	echo $degree."<br>";
+	echo $blood."<br>";
+	
+?>
